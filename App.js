cc111eecdd9361ceac000946219b2052621bb662
John Linatoc <johnlinatoc1227@gmail.com>
2019-11-03 14:32:22 -0600

    Add state and render input into state

--- App.js
+++ App.js
@@ -8,7 +8,41 @@ export default class App extends React.Component {
     inputValue: ''
   };
 
+  changeText = value => {
+    this.setState({
+      inputValue: value,
+      todos: [],
+    })
+  }
+
+  addItem = () => {
+    if (this.state.inputValue != ''){
+      this.setState( prevState => {
+        const newToDo = {
+          title: this.state.inputValue,
+          createdAt: Date.now(),
+        };
+
+        var todos = this.state.todos.concat(newToDo);
+
+        this.setState({ todos })
+      })
+    }
+  }
+
   render() {
+    const todos = this.state.todos.reverse().map((todo, key) =>
+      <View style={{ flexDirection: 'row', marginTop: 20 }}>
+      <TouchableOpacity style={{
+        width: 20,
+        height: 20,
+        borderRadius: 20,
+        borderWidth: 30,
+        borderColor: 'white',
+        margin: 15
+      }}>
+    )
+
     return (
         <LinearGradient colors={['#667eea', '#764ba2']} style={{ flex:1 }}>
           <StatusBar barStyle="light-content" />
@@ -17,6 +51,8 @@ export default class App extends React.Component {
               style={styles.input}
               placeholder="Type here to add a to do."
               placeholderTextColor={'#fff'}
+              onChangeText={this.changeText}
+              value={this.state.inputValue}
               multiline={true}
               autoCapitalize="sentences"
               underlineColorAndroid="transparent"
